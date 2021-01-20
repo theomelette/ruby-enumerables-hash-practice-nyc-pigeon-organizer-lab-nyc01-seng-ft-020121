3abe0175607668cf5e0cacc41d1b11c3bedad9d2
Amit Naipaul <amitnaipaul@gmail.com>
2021-01-20 16:21:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,15 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |attribute, names|
-    names.each do |name|
-      if !new_hash[name]
-        new_hash[name] = {}
+  data.each do |key, value|
+    value.each do |attribute, names|
+      names.each do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][key]
+          new_hash[name][key] = []
       end
-      if !new_hash[name][key]
-        new_hash[name][key] = []
-      end
-      new_hash[name][key] << attribute.to_s
+        new_hash[name][key] << attribute.to_s
     end
   end
 end
